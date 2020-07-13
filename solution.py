16b82caa317f72bb065042cba4c837f6e4c1c1bb
rnv19 <routulasundeep@msitprogram.net>
2020-07-13 12:47:22 +0530

    Jul 13  12:47:22

--- solution.py
+++ solution.py
@@ -2,6 +2,15 @@
 # The program should return true or false
 
 def checkArmstrong(num):
-        # Your code goes here
-
-       return True
+    # Your code goes here
+    total = 0
+    temp = num
+    while(num == 0):
+        a = num % 10
+        num = num / 10
+        total = a**2
+        
+    if(total == temp):
+        return True
+    else:
+        return fase
